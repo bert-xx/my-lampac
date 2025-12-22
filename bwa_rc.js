50bab837b4486c8c5842017c1cfa7444792c93f9
bert-xx <nowertx@gmail.com>
2025-12-22 09:35:07 +0300

    bwa_rc.js

--- bwa_rc.js
+++ bwa_rc.js
@@ -3,14 +3,14 @@
 
   var Defined = {
     api: 'lampac',
-    localhost: 'http://arkmv.ru:2053/', // Сервер-парсер
+    // ЗАМЕНИТЕ ЭТУ ССЫЛКУ на ваш адрес из Koyeb (слэш в конце обязателен!)
+    localhost: 'https://valuable333.koyeb.app/', 
     apn: ''
   };
 
-  // Статичный ID, чтобы сервер не мог идентифицировать вас лично
-  var unic_id = 'private_user';
+  var unic_id = Lampa.Storage.get('lampac_unic_id', Lampa.Utils.uid(8));
 
-  // Очищенная функция подготовки ссылок (без Email и Токенов)
+  // Функция подготовки ссылок (чистая, без шпионажа)
   function account(url) {
     url = url + '';
     if (url.indexOf('uid=') == -1) {
@@ -18,36 +18,34 @@
     }
     return url;
   }
-  
-  var Network = Lampa.Reguest;
 
-  // Логика отображения онлайн-меню
   function component(object) {
-    var network = new Network();
+    var network = new Lampa.Reguest();
     var scroll = new Lampa.Scroll({ mask: true, over: true });
     var files = new Lampa.Explorer(object);
     var filter = new Lampa.Filter(object);
     var sources = {};
     var balanser;
     var filter_sources = [];
-    var images = [];
 
     this.initialize = function() {
       var _this = this;
       this.loading(true);
       
-      // Запрос списка балансеров у сервера
+      // Запрашиваем балансеры у ВАШЕГО Lampac
       var url = this.requestParams(Defined.localhost + 'lite/events');
       network.silent(account(url), function(json) {
         if (json.online) {
           json.online.forEach(function(j) {
-            sources[j.name.toLowerCase()] = { url: j.url, name: j.name, show: true };
+            var name = (j.balanser || j.name.split(' ')[0]).toLowerCase();
+            sources[name] = { url: j.url, name: j.name };
           });
           filter_sources = Object.keys(sources);
-          balanser = filter_sources[0];
+          balanser = Lampa.Storage.get('online_balanser', filter_sources[0]);
+          if(!sources[balanser]) balanser = filter_sources[0];
           _this.find();
-        }
-      }, function() { _this.loading(false); });
+        } else { _this.empty(); }
+      }, function() { _this.empty(); });
     };
 
     this.requestParams = function(url) {
@@ -55,6 +53,7 @@
       query.push('id=' + encodeURIComponent(object.movie.id));
       query.push('title=' + encodeURIComponent(object.movie.title || object.movie.name));
       query.push('serial=' + (object.movie.name ? 1 : 0));
+      query.push('year=' + ((object.movie.release_date || object.movie.first_air_date || '0000') + '').slice(0, 4));
       return url + (url.indexOf('?') >= 0 ? '&' : '?') + query.join('&');
     };
 
@@ -66,71 +65,62 @@
       var _this = this;
       network.native(account(url), function(str) {
         _this.parse(str);
-      }, function() { _this.loading(false); }, false, { dataType: 'text' });
+      }, function() { _this.empty(); }, false, { dataType: 'text' });
     };
 
     this.parse = function(str) {
       this.loading(false);
-      // Тут стандартная отрисовка карточек серий и фильмов
-      // (Оставлена оригинальная логика Lampa для построения списка)
-      var items = this.parseJsonDate(str, '.videos__item');
-      if (items.length) this.display(items);
-      else this.empty();
-    };
-
-    this.parseJsonDate = function(str, name) {
       try {
         var html = $('<div>' + str + '</div>');
-        var elems = [];
-        html.find(name).each(function() {
+        var items = [];
+        html.find('.videos__item').each(function() {
           var data = JSON.parse($(this).attr('data-json'));
           data.text = $(this).text();
-          elems.push(data);
+          items.push(data);
         });
-        return elems;
-      } catch (e) { return []; }
+        if (items.length) this.display(items);
+        else this.empty();
+      } catch (e) { this.empty(); }
     };
 
     this.display = function(items) {
       var _this = this;
+      scroll.clear();
+      items.forEach(function(element) {
+        var html = Lampa.Template.get('button', { title: element.text, description: element.quality || '' });
+        html.on('hover:enter', function() {
+          if (element.method == 'play' || element.url) {
+            Lampa.Player.play({ url: element.url, title: element.text, movie: object.movie });
+            _this.mark(element);
+          }
+        });
+        scroll.append(html);
+      });
       Lampa.Controller.enable('content');
-      // Отрисовка элементов в скролл...
     };
 
-    this.loading = function(status) {
-      this.activity.loader(status);
-      if (!status) this.activity.toggle();
-    };
-
+    this.mark = function(elem) { /* Логика отметки просмотра */ };
+    this.empty = function() { this.loading(false); scroll.clear(); scroll.append($('<div class="empty">На вашем сервере Lampac ничего не найдено</div>')); };
+    this.loading = function(status) { this.activity.loader(status); if(!status) this.activity.toggle(); };
     this.render = function() { return files.render(); };
     this.destroy = function() { network.clear(); scroll.destroy(); };
   }
 
-  // Регистрация плагина в Лампе
   function startPlugin() {
     window.vod_plugin = true;
     Lampa.Component.add('vod', component);
+    Lampa.Manifest.plugins = { type: 'video', name: 'My Private Online', version: '1.0.0' };
 
     Lampa.Listener.follow('full', function(e) {
       if (e.type == 'complite') {
-        var btn = $('<div class="full-start__button selector view--online"><span>Смотреть Онлайн (Безопасно)</span></div>');
+        var btn = $('<div class="full-start__button selector view--online"><span>Смотреть на моем сервере</span></div>');
         btn.on('hover:enter', function() {
-          Lampa.Activity.push({
-            component: 'vod',
-            movie: e.data.movie,
-            title: 'Онлайн'
-          });
+          Lampa.Activity.push({ component: 'vod', movie: e.data.movie, title: 'Мой онлайн' });
         });
         e.render.after(btn);
       }
     });
-
-    // Стили для интерфейса
-    Lampa.Template.add('lampac_css', "<style>.online-prestige{ background:rgba(0,0,0,0.4); border-radius:4px; padding:10px; }</style>");
-    $('body').append(Lampa.Template.get('lampac_css', {}, true));
   }
 
   if (!window.vod_plugin) startPlugin();
-
-  // ВСЕ ШПИОНСКИЕ СКРИПТЫ, МЕТРИКИ И RCH УДАЛЕНЫ
 })();
